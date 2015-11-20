d7950b9dc70d61636986c3b1fd877a28522dd0fb
Albert <albert.yu@gmail.com>
2015-11-19 22:50:45 -0500

    added to_string method to conflict class

--- docx_writer.py
+++ docx_writer.py
@@ -7,14 +7,14 @@ __author__ = 'Albert Yu'
 # Date: 10-15-15
 
 
-def create_document(current_date):
+def create_document(date):
     """
     Creates the word document
-    :param current_date, a datetime object
+    :param date, a datetime object
     :return: none
     """
     document = Document()
-    date_as_string = current_date.month.__str__() + "-" + current_date.day.__str__() + "-" + current_date.year.__str__()
+    date_as_string = date.month.__str__() + "-" + date.day.__str__() + "-" + date.year.__str__()
     document.add_heading('CONFLICTS ' + date_as_string, 0)
     table = document.add_table(rows=1, cols=3)
     hdr_cells = table.rows[0].cells
--- email_writer.py
+++ email_writer.py
@@ -6,10 +6,11 @@ __version__ = '0.1'
 # This will send an email from asoconflicts@gmail.com to mlswanson@amherst.edu every time a conflict is reported
 
 
-def write_email(password):
+def write_email(password, date):
     """
     Connects to mail server, writes the email, and quits.
     :param password: the password, which will be passed as a raw input
+    :param date: the date to which the conflicts pertain
     :return: nothing
     """
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
@@ -17,7 +18,8 @@ def write_email(password):
     smtpObj.starttls()
     smtpObj.login('asoconflicts@gmail.com', password)
     smtpObj.sendmail(' asoconflicts@gmail.com ', ' mlswanson@amherst.edu ',
-                     'Subject: So long.\nDear Alice, so long and thanks for all the fish. Sincerely, Bob')
+                     'Subject: Conflicts today' + date + '\n' +
+                     'Dear Mark, here is a test message.')
     smtpObj.quit()
 
 
