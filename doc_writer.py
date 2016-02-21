dda616d48d8c3bee6ea3957dcbab7d0df406c7c1
Albert <albert.yu@gmail.com>
2016-02-21 00:37:57 -0500

    Add new script to generate doc for week's conflicts

--- doc_writer.py
+++ doc_writer.py
@@ -4,18 +4,41 @@ from parser import *
 __author__ = 'Albert Yu'
 
 
-def main():
-    date = datetime.date.today()
-    document = Document()
-    date_as_string = date.month.__str__() + "-" + date.day.__str__() + "-" + date.year.__str__()
-    document.add_heading('CONFLICTS ' + date_as_string, 0)
-    table = document.add_table(rows=1, cols=4)
-    headings = table.rows[0].cells
+def add_conflict_row(conflict, row):
+    row[0].text = conflict.name
+    row[1].text = conflict.instrument
+    row[2].text = conflict.time
+    row[3].text = conflict.reason
+
+
+def create_headings(headings):
     headings[0].text = 'Name'
     headings[1].text = 'Instrument'
     headings[2].text = 'Time'
     headings[3].text = 'Reason'
+
+
+def create_doc(heading_pre, date_as_string):
+    """
+    Creates a document with an empty table and given heading
+    :param heading_pre: string, the prefix of the heading (e.g. 'Conflicts ')
+    :param date_as_string: string, the date as a string
+    :return:
+    """
+    document = Document()
+    document.add_heading(heading_pre + date_as_string, 0)
+    table = document.add_table(rows=1, cols=4)
+    headings = table.rows[0].cells
+    create_headings(headings)
+    return document
+
+
+def main():
+    date = datetime.date.today()
+    date_as_string = date.month.__str__() + "-" + date.day.__str__() + "-" + date.year.__str__()
+    document = create_doc('CONFLICTS ', date_as_string)
     file_name = 'Conflicts ' + date_as_string + '.docx'
+    table = document.tables[0]
 
     # add ongoing conflicts first
     ongoing = store_conflicts('ongoing.csv')
---
+++ weekly_preview.py
@@ -0,0 +1,66 @@
+from doc_writer import *
+from dateutil import rrule
+
+__author__ = 'Albert Yu'
+
+
+# Previews upcoming conflicts for the coming week. Run this every Sunday before rehearsal.
+
+
+def store_week_conflicts(csv_filename):
+    conflicts = []
+    date = datetime.date.today()
+    end_of_week = date + datetime.timedelta(days=7)
+    date_range = [dt.date() for dt in rrule.rrule(rrule.DAILY, dtstart=date, until=end_of_week)]
+    with open(csv_filename, "rt") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] != "Timestamp":
+                if string_to_datetime(row[3]) in date_range:
+                    conflicts.append(create_conflict(row))
+    return conflicts
+
+
+def main():
+    date = datetime.date.today()
+    date_as_string = date.month.__str__() + "-" + date.day.__str__() + "-" + date.year.__str__()
+
+    document = Document()
+    document.add_heading('CONFLICTS for the week of ' + date_as_string, 0)
+    document.add_heading('Tuesday', level=2)
+    table_tues = document.add_table(rows=1, cols=4)
+    headings_tues = table_tues.rows[0].cells
+    create_headings(headings_tues)
+
+    document.add_heading('Thursday', level=2)
+    table_thurs = document.add_table(rows=1, cols=4)
+    # headings_thurs = table_thurs.rows[0].cells
+    # create_headings(headings_thurs)
+
+    # first iterate through ongoing conflicts
+    ongoing = store_conflicts('ongoing.csv')
+    for conflict in ongoing:
+        if conflict.date[:len(conflict.date) - 1] == "Tuesday":
+            row = table_tues.add_row().cells
+            add_conflict_row(conflict, row)
+        elif conflict.date[:len(conflict.date) - 1] == "Thursday":
+            row = table_thurs.add_row().cells
+            add_conflict_row(conflict, row)
+
+    conflicts = store_week_conflicts('Conflicts.csv')
+    for conflict in conflicts:
+        # if the weekday falls on a Tuesday, append it to the Tuesday table
+        if string_to_datetime(conflict.date).weekday() == 1:
+            row = table_tues.add_row().cells
+            add_conflict_row(conflict, row)
+        # if Thursday, append to Thursday table
+        elif string_to_datetime(conflict.date).weekday() == 3:
+            row = table_thurs.add_row().cells
+            add_conflict_row(conflict, row)
+
+    file_name = 'Conflicts week of ' + date_as_string + '.docx'
+    document.save(file_name)
+
+
+if __name__ == main():
+    main()
